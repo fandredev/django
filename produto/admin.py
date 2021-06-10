08e0d5bebeb97ff73312f0c3979fe3fee27704cf
felipe <profissionalf.andre@gmail.com>
2021-06-10 18:49:41 -0300

    first commit

---
+++ produto/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django.http.response import HttpResponse
+
+# Register your models here.
---
+++ produto/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.http.response import HttpResponse
+
+# Create your models here.
